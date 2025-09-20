7dc3b59a5df3022809b0f63863c424489b3e9d89
berkatsiallagan <berkatsiallagan201105@gmail.com>
2025-09-21 02:12:58 +0700

    Menyimpan logo dan favicon.io QParkin

--- qparkin_backend/config/app.php
+++ qparkin_backend/config/app.php
@@ -14,6 +14,8 @@ return [
     */
 
     'name' => env('APP_NAME', 'Laravel'),
+    'logo' => env('APP_LOGO', 'images/logo.png'),
+    'favicon' => env('APP_FAVICON', 'images/favicon.ico'),
 
     /*
     |--------------------------------------------------------------------------
