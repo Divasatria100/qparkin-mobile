6c61b39508f2df181d15983db9c00df9eebedf3b
Diva Satria <divasatria700@gmail.com>
2025-11-21 20:55:56 +0700

    Memperbarui bottom navigasi

--- qparkin_app/lib/presentation/widgets/bottom_nav.dart
+++ qparkin_app/lib/presentation/widgets/bottom_nav.dart
@@ -1,9 +1,4 @@
-import 'dart:math';
-
-import '../../src/nav_custom_clipper.dart';
 import 'package:flutter/material.dart';
-import '../../src/nav_button.dart';
-import '../../src/nav_custom_painter.dart';
 
 typedef _LetIndexPage = bool Function(int value);
 
@@ -16,8 +11,6 @@ class CurvedNavigationBar extends StatefulWidget {
   final Color backgroundColor;
   final ValueChanged<int>? onTap;
   final _LetIndexPage letIndexChange;
-  final Curve animationCurve;
-  final Duration animationDuration;
   final double height;
   final double? maxWidth;
 
@@ -26,25 +19,21 @@ class CurvedNavigationBar extends StatefulWidget {
     this.icons = const [
       Icons.home,
       Icons.history,
-      Icons.qr_code_scanner,
       Icons.notifications,
       Icons.person,
     ],
     this.labels = const [
       'Beranda',
       'Aktivitas',
-      'SCAN',
       'Notifikasi',
       'Profil',
     ],
     this.index = 0,
     this.color = Colors.white,
     this.buttonBackgroundColor = const Color(0xFFFB923C),
-    this.backgroundColor = const Color.fromARGB(0, 106, 99, 142),
+    this.backgroundColor = Colors.white,
     this.onTap,
     _LetIndexPage? letIndexChange,
-    this.animationCurve = Curves.easeOut,
-    this.animationDuration = const Duration(milliseconds: 600),
     this.height = 75.0,
     this.maxWidth,
   })  : letIndexChange = letIndexChange ?? ((_) => true),
@@ -60,159 +49,61 @@ class CurvedNavigationBar extends StatefulWidget {
   CurvedNavigationBarState createState() => CurvedNavigationBarState();
 }
 
-class CurvedNavigationBarState extends State<CurvedNavigationBar>
-    with SingleTickerProviderStateMixin {
-  late double _startingPos;
-  late int _endingIndex;
-  late double _pos;
-  double _buttonHide = 0;
-  late Widget _icon;
-  late AnimationController _animationController;
-  late int _length;
-
-  @override
-  void initState() {
-    super.initState();
-    _icon = Icon(widget.icons[widget.index], size: 24, color: Colors.white);
-    _length = widget.icons.length;
-    _pos = widget.index / _length;
-    _startingPos = widget.index / _length;
-    _endingIndex = widget.index;
-    _animationController = AnimationController(vsync: this, value: _pos);
-    _animationController.addListener(() {
-      setState(() {
-        _pos = _animationController.value;
-        final endingPos = _endingIndex / widget.icons.length;
-        final middle = (endingPos + _startingPos) / 2;
-        if ((endingPos - _pos).abs() < (_startingPos - _pos).abs()) {
-          _icon = Icon(widget.icons[_endingIndex], size: 24, color: Colors.white);
-        }
-        _buttonHide =
-            (1 - ((middle - _pos) / (_startingPos - middle)).abs()).abs();
-      });
-    });
-  }
-
-  @override
-  void didUpdateWidget(CurvedNavigationBar oldWidget) {
-    super.didUpdateWidget(oldWidget);
-    if (oldWidget.index != widget.index) {
-      final newPosition = widget.index / _length;
-      _startingPos = _pos;
-      _endingIndex = widget.index;
-      _animationController.animateTo(newPosition,
-          duration: widget.animationDuration, curve: widget.animationCurve);
-    }
-    if (!_animationController.isAnimating) {
-      _icon = Icon(widget.icons[_endingIndex], size: 24, color: Colors.white);
-    }
-  }
-
-  @override
-  void dispose() {
-    _animationController.dispose();
-    super.dispose();
-  }
+class CurvedNavigationBarState extends State<CurvedNavigationBar> {
 
   @override
   Widget build(BuildContext context) {
-    final textDirection = Directionality.of(context);
     return SizedBox(
       height: widget.height,
       child: LayoutBuilder(
         builder: (context, constraints) {
-          final maxWidth = min(
-              constraints.maxWidth, widget.maxWidth ?? constraints.maxWidth);
-          return Align(
-            alignment: textDirection == TextDirection.ltr
-                ? Alignment.bottomLeft
-                : Alignment.bottomRight,
-            child: Container(
-              width: maxWidth,
-              decoration: BoxDecoration(
-                color: widget.backgroundColor,
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(20),
-                  topRight: Radius.circular(20),
-                ),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black.withOpacity(0.2),
-                    blurRadius: 10,
-                    offset: Offset(0, -5),
-                  ),
-                ],
+          final maxWidth = widget.maxWidth ?? constraints.maxWidth;
+          return Container(
+            width: maxWidth,
+            decoration: BoxDecoration(
+              color: widget.backgroundColor,
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(20),
+                topRight: Radius.circular(20),
               ),
-              child: Stack(
-                clipBehavior: Clip.none,
-                alignment: Alignment.bottomCenter,
-                children: <Widget>[
-                  Positioned(
-                    bottom: -40 - (75.0 - widget.height),
-                    left: textDirection == TextDirection.rtl
-                        ? null
-                        : _pos * maxWidth,
-                    right: textDirection == TextDirection.rtl
-                        ? _pos * maxWidth
-                        : null,
-                    width: maxWidth / _length,
-                    child: Center(
-                      child: Transform.translate(
-                        offset: Offset(
-                          0,
-                          -(1 - _buttonHide) * 80,
-                        ),
-                          child: Material(
-                            color: widget.buttonBackgroundColor ?? widget.color,
-                            type: MaterialType.circle,
-                            child: Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: _icon,
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.2),
+                  blurRadius: 10,
+                  offset: Offset(0, -5),
+                ),
+              ],
+            ),
+            child: Row(
+              children: List.generate(widget.icons.length, (index) {
+                final isActive = index == widget.index;
+                return Expanded(
+                  child: GestureDetector(
+                    onTap: () => _buttonTap(index),
+                    child: Container(
+                      color: Colors.transparent,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(
+                            widget.icons[index],
+                            size: 24,
+                            color: isActive ? widget.buttonBackgroundColor : Colors.black,
+                          ),
+                          const SizedBox(height: 4),
+                          Text(
+                            widget.labels[index],
+                            style: TextStyle(
+                              fontSize: 10,
+                              color: isActive ? widget.buttonBackgroundColor : Colors.black,
                             ),
                           ),
+                        ],
                       ),
                     ),
                   ),
-                  Positioned(
-                    left: 0,
-                    right: 0,
-                    bottom: 0 - (75.0 - widget.height),
-                    child: CustomPaint(
-                      painter: NavCustomPainter(
-                          _pos, _length, widget.color, textDirection),
-                      child: Container(
-                        height: 75.0,
-                      ),
-                    ),
-                  ),
-                  Positioned(
-                    left: 0,
-                    right: 0,
-                    bottom: 0 - (75.0 - widget.height),
-                    child: SizedBox(
-                        height: 100.0,
-                        child: Row(
-                            children: List.generate(widget.icons.length, (index) {
-                          final isActive = index == widget.index;
-                          final item = Column(
-                            mainAxisSize: MainAxisSize.min,
-                            children: [
-                              Icon(widget.icons[index], size: 24, color: isActive ? Colors.white : Colors.black),
-                              const SizedBox(height: 4),
-                              Text(widget.labels[index], style: TextStyle(fontSize: 10, color: isActive ? Colors.white : Colors.black)),
-                            ],
-                          );
-                          return NavButton(
-                            onTap: _buttonTap,
-                            position: _pos,
-                            length: _length,
-                            index: index,
-                            child: Center(child: item),
-                          );
-                        }))),
-                  ),
-                ],
-              ),
+                );
+              }),
             ),
           );
         },
@@ -225,18 +116,86 @@ class CurvedNavigationBarState extends State<CurvedNavigationBar>
   }
 
   void _buttonTap(int index) {
-    if (!widget.letIndexChange(index) || _animationController.isAnimating) {
+    if (!widget.letIndexChange(index)) {
       return;
     }
     if (widget.onTap != null) {
       widget.onTap!(index);
     }
-    final newPosition = index / _length;
-    setState(() {
-      _startingPos = _pos;
-      _endingIndex = index;
-      _animationController.animateTo(newPosition,
-          duration: widget.animationDuration, curve: widget.animationCurve);
-    });
+    setState(() {});
+  }
+}
+
+class BottomNavWithFab extends StatelessWidget {
+  final int currentIndex;
+  final ValueChanged<int> onTap;
+  final VoidCallback onFabPressed;
+
+  const BottomNavWithFab({
+    Key? key,
+    required this.currentIndex,
+    required this.onTap,
+    required this.onFabPressed,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(), // Placeholder, actual body will be in parent Scaffold
+      floatingActionButton: FloatingActionButton(
+        onPressed: onFabPressed,
+        backgroundColor: const Color(0xFFFB923C),
+        child: const Icon(Icons.qr_code_scanner, color: Colors.white),
+        elevation: 6,
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      bottomNavigationBar: BottomAppBar(
+        shape: const CircularNotchedRectangle(),
+        notchMargin: 8,
+        child: SizedBox(
+          height: 75.0,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              _buildNavItem(0, Icons.home, 'Beranda'),
+              _buildNavItem(1, Icons.history, 'Aktivitas'),
+              const SizedBox(width: 48), // Space for FAB
+              _buildNavItem(2, Icons.notifications, 'Notifikasi'),
+              _buildNavItem(3, Icons.person, 'Profil'),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildNavItem(int index, IconData icon, String label) {
+    final isActive = index == currentIndex;
+    return Expanded(
+      child: GestureDetector(
+        onTap: () => onTap(index),
+        child: Container(
+          color: Colors.transparent,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(
+                icon,
+                size: 24,
+                color: isActive ? const Color(0xFFFB923C) : Colors.black,
+              ),
+              const SizedBox(height: 4),
+              Text(
+                label,
+                style: TextStyle(
+                  fontSize: 10,
+                  color: isActive ? const Color(0xFFFB923C) : Colors.black,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
